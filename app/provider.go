56f7a599ecec5e01f4c0265692d67de378a0e769
Saurabh Mishra <saurabh.mish@icloud.com>
2022-05-17 11:33:25 -0700

    resources to connect to API endpoint

---
+++ app/provider.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+// Resources required to maintain connection with the API endpoint
+func Provider() terraform.ResourceProvider {
+	return &schema.Provider {
+		Schema: map[string]*schema.Schema{
+			"token": {
+				Type:     schema.TypeString,
+				Required: true
+				Description: "JSON web token (JWT)"
+				DefaultFunc: schema.EnvDefaultFunc("CONCOURSE_TOKEN", "")
+			},
+		},
+		configureFunc: configureProvider,
+	}
+}
+
+// Resources required to create connection with the API endpoint
+func configureProvider(d *schema.ResourceData) (interface{}, error) {
+	token := d.Get(key: "token").(string)
+	return client.NewClient(token), nil
+}
