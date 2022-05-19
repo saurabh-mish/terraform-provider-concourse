10552687979da8878d4a7fadef6b6469e49976fd
Saurabh Mishra <saurabh.mish@icloud.com>
2022-05-19 11:50:25 -0700

    creates four orders - two of each coffee_id '3' and '2'

---
+++ create.tf
@@ -0,0 +1,18 @@
+resource "hashicups_order" "edu" {
+  items {
+    coffee {
+      id = 3
+    }
+    quantity = 2
+  }
+  items {
+    coffee {
+      id = 2
+    }
+    quantity = 2
+  }
+}
+
+output "edu_order" {
+  value = hashicups_order.edu
+}
