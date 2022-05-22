18b1d283d1fd29fde91d729bb4198c744b7baf63
Saurabh Mishra <saurabh.mish@icloud.com>
2022-05-22 08:46:42 -0700

    provider has read functionality

--- hashicups/provider.go
+++ hashicups/provider.go
@@ -1,13 +1,16 @@
 package hashicups
 
 import (
-  "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // Provider -
 func Provider() *schema.Provider {
-  return &schema.Provider{
-    ResourcesMap: map[string]*schema.Resource{},
-    DataSourcesMap: map[string]*schema.Resource{},
-  }
+	return &schema.Provider{
+		ResourcesMap: map[string]*schema.Resource{},
+		// DataSourcesMap: map[string]*schema.Resource{},
+		DataSourcesMap: map[string]*schema.Resource{
+			"hashicups_coffees": dataSourceCoffees(),
+		},
+	}
 }
