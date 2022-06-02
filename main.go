1821e6d616f9690e54f3b27fca6832ea95ec180e
Saurabh Mishra <saurabh.mish@icloud.com>
2022-06-02 14:07:10 -0700

    call to relevant method

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"github.com/saurabh-mish/terraform-provider-concourse/client"
 )
 
-func main() {
-	username, password := client.CheckCredentials()
-	//fmt.Printf("Concourse Email:    %s\n", *username)
-	//fmt.Printf("Concourse Password: %s\n", *password)
 
-	respData := client.GetAuthData(username, password)
+//WORKS with overcloud.go
+func main() {
+	username := os.Getenv("CONCOURSE_USERNAME")
+	password := os.Getenv("CONCOURSE_PASSWORD")
+	respData, val := client.NewClient(&username, &password)
+	if val != nil {
+		fmt.Println(val)
+	}
 	fmt.Println(respData)
 }
+
+
+/*
+func main() {
+
+}
+*/
