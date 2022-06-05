7c11f889ab91a5c7b6d3e060efc701d860243f79
Saurabh Mishra <saurabh.mish@icloud.com>
2022-06-04 17:26:47 -0700

    terraform provider

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+/*
 package main
 
 import (
@@ -7,20 +8,31 @@ import (
 )
 
 
-//WORKS with overcloud.go
 func main() {
 	username := os.Getenv("CONCOURSE_USERNAME")
 	password := os.Getenv("CONCOURSE_PASSWORD")
-	respData, val := client.NewClient(&username, &password)
+	hosturl  := "https://auth.prod.concourselabs.io/api/v1/oauth"
+	respData, val := client.NewClient(&hosturl, &username, &password)
 	if val != nil {
 		fmt.Println(val)
 	}
 	fmt.Println(respData)
 }
-
-
-/*
-func main() {
-
-}
 */
+
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+
+	"github.com/saurabh-mish/terraform-provider-concourse/concourse"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return concourse.Provider()
+		},
+	})
+}
