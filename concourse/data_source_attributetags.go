df159754d2c45525eaf2f8d8f6da59e6e9f95055
Saurabh Mishra <saurabh.mish@icloud.com>
2022-06-07 10:37:54 -0700

    set data to defined fields

--- concourse/data_source_attributetags.go
+++ concourse/data_source_attributetags.go
@@ -29,15 +29,15 @@ func dataSourceAttributeTag() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
-			"createdBy": &schema.Schema{
+			"created_by": &schema.Schema{
 				Type:     schema.TypeInt,
 				Computed: true,
 			},
-			"updatedBy": &schema.Schema{
+			"updated_by": &schema.Schema{
 				Type:     schema.TypeInt,
 				Computed: true,
 			},
-			"institutionId": &schema.Schema{
+			"institution_id": &schema.Schema{
 				Type:     schema.TypeInt,
 				Computed: true,
 			},
@@ -61,48 +61,24 @@ func dataSourceAttributeTagRead(ctx context.Context, d *schema.ResourceData, m i
 
 	tagID := strconv.Itoa(d.Get("id").(int))
 
-	_, err := c.GetAttributeTag(tagID)
+	resp, err := c.GetAttributeTag(tagID)
 	if err != nil {
 		return diag.FromErr(err)
 	}
+
+  d.Set("id", resp.ID)
+  d.Set("version", resp.Version)
+  d.Set("created", resp.Created)
+  d.Set("updated", resp.Updated)
+  d.Set("createdBy", resp.CreatedBy)
+  d.Set("updatedBy", resp.UpdatedBy)
+  d.Set("institutionId", resp.InstitutionId)
+  d.Set("name", resp.Name)
+  d.Set("description", resp.Description)
+
 
 	// set response body
 	d.SetId(tagID)
 
 	return diags
 }
-
-/*
-func dataSourceAttributeTagRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-  client := &http.Client{Timeout: 10 * time.Second}
-
-  // Warning or errors can be collected in a slice type
-  var diags diag.Diagnostics
-
-  req, err := http.NewRequest("GET", fmt.Sprintf("%s/coffees", "http://localhost:19090"), nil)
-  if err != nil {
-    return diag.FromErr(err)
-  }
-
-  r, err := client.Do(req)
-  if err != nil {
-    return diag.FromErr(err)
-  }
-  defer r.Body.Close()
-
-  coffees := make([]map[string]interface{}, 0)
-  err = json.NewDecoder(r.Body).Decode(&coffees)
-  if err != nil {
-    return diag.FromErr(err)
-  }
-
-  if err := d.Set("coffees", coffees); err != nil {
-    return diag.FromErr(err)
-  }
-
-  // always run
-  d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
-
-  return diags
-}
-*/
