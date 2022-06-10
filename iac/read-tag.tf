7c6d984f8b85bed473f3171f3d6b4d6688895b64
Saurabh Mishra <saurabh.mish@icloud.com>
2022-06-10 09:14:56 -0700

    test read from create

--- iac/read-tag.tf
+++ iac/read-tag.tf
@@ -1,24 +1,32 @@
-terraform {
-  required_providers {
-    concourse = {
-      version = "0.3.1"
-      source = "hashicorp.com/edu/concourse"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     concourse = {
+#       version = "0.3.1"
+#       source = "hashicorp.com/edu/concourse"
+#     }
+#   }
+# }
 
-data "concourse_attribute_tag" "freddie_mac" {
-  id = 211012
-}
+# data "concourse_attribute_tag" "freddie_mac" {
+#   id = 211012
+# }
 
-data "concourse_attribute_tag" "first_tag" {
-  id = 60003
-}
+# data "concourse_attribute_tag" "first_tag" {
+#   id = 60003
+# }
 
-output "freddie" {
-  value = data.concourse_attribute_tag.freddie_mac
-}
+# data "concourse_attribute_tag" "test_tag" {
+#   id = 214148
+# }
 
-output "oldest" {
-  value = data.concourse_attribute_tag.first_tag
-}
+# output "freddie" {
+#   value = data.concourse_attribute_tag.freddie_mac
+# }
+
+# output "oldest" {
+#   value = data.concourse_attribute_tag.first_tag
+# }
+
+# output "saurabh" {
+#   value = data.concourse_attribute_tag.test_tag
+# }
